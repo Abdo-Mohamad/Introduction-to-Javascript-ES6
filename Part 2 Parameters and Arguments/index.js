612333dac3eaf2526e092920abec1336a38955cf
Abdo Mohamad <abdomohamadwalid@gmail.com.com>
2024-06-13 16:14:38 +0200

    Functions Part 2: Parameters and Arguments

---
+++ Part 2 Parameters and Arguments/index.js
@@ -0,0 +1,24 @@
+function getMilk(monye) {
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("buyMilk you can by " + Math.floor(monye / 1.5) + " bottles of milk");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+
+}
+
+getMilk(prompt("enter the monye"));
